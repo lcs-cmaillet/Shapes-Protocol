df97308572c9d103ea97c5b1b0f1229298f5299a
Collin Maillet <cmaillet@lcs.on.ca>
2024-11-26 14:27:25 -0500

    Setted up project

---
+++ Shapes Protocol/Shapes/Arrow.swift
@@ -0,0 +1,19 @@
+//
+//  Arrow.swift
+//  Shapes Protocol
+//
+//  Created by Collin Maillet on 2024-11-26.
+//
+
+import SwiftUI
+
+struct Arrow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Arrow()
+        .padding()
+}
