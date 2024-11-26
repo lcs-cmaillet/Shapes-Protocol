8d22d95a568497106c27314e91d66ff80f6c38c5
Collin Maillet <cmaillet@lcs.on.ca>
2024-11-26 14:25:26 -0500

    Initial Commit

---
+++ Shapes Protocol/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shapes Protocol
+//
+//  Created by Collin Maillet on 2024-11-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shapes Protocol/Shapes_ProtocolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shapes_ProtocolApp.swift
+//  Shapes Protocol
+//
+//  Created by Collin Maillet on 2024-11-26.
+//
+
+import SwiftUI
+
+@main
+struct Shapes_ProtocolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
