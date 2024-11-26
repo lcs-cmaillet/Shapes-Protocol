df89099e7fc63713171c3ff8a3c1b75677da599b
Collin Maillet <cmaillet@lcs.on.ca>
2024-11-26 14:57:07 -0500

    framed ribbon in content view

--- Shapes Protocol/Views/ContentView.swift
+++ Shapes Protocol/Views/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Ribbon()
-            Ribbon()
+                .frame(height:100)
         }
         .padding()
     }
